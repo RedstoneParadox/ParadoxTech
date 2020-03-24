886460d932e70187a81ba3f91ab8b3f6b923a084
RedstoneParadox <spinjitzu00@gmail.com>
2020-03-24 16:20:11 -0700

    Added compressed and hot air.

--- prototypes/fluid.lua
+++ prototypes/fluid.lua
@@ -22,7 +22,31 @@ local molten_copper = {
     flow_color = {r = 0.402, g = 0.068, b = 0.020, a = 1}
 }
 
+local compressed_air = {
+    type = "fluid",
+    name = "molten-copper",
+    icon = "__ParadoxTech__/graphics/icons/molten_copper.png",
+    icon_size = 32,
+    default_temperature = 20,
+    max_temperature = 1000,
+    base_color = {r = 1, g = 1, b = 1, a = 0.3},
+    flow_color = {r = 1, g = 1, b = 1, a = 0.3}
+}
+
+local hot_air = {
+    type = "fluid",
+    name = "molten-copper",
+    icon = "__ParadoxTech__/graphics/icons/molten_copper.png",
+    icon_size = 32,
+    default_temperature = 1000,
+    max_temperature = 5000,
+    base_color = {r = 1, g = 151/255, b = 74/255, a = 0.5},
+    flow_color = {r = 1, g = 151/255, b = 74/255, a = 0.5}
+}
+
 data:extend{
     molten_iron,
-    molten_copper
+    molten_copper,
+    compressed_air,
+    hot_air
 }
