be737832bc575e7a3d1fbce924a988895d85980b
RedstoneParadox <spinjitzu00@gmail.com>
2020-03-24 16:37:52 -0700

    Changed fluid icons to use vanilla ones with tint

--- prototypes/fluid.lua
+++ prototypes/fluid.lua
@@ -1,10 +1,17 @@
 -- Fluids
 
+local dropletIcon = data.raw.fluid["crude-oil"].icon
+
 local molten_iron = {
     type = "fluid",
     name = "molten-iron",
-    icon = "__ParadoxTech__/graphics/icons/molten_iron.png",
-    icon_size = 32,
+    icons = {
+        {
+            icon = table.deepcopy(dropletIcon),
+            tint = {r = 1, g = 64/255, b = 0, a = 1},
+            icon_size = 32
+        }
+    },
     default_temperature = 1538,
     max_temperature = 2862,
     base_color = {r = 0.402, g = 0.036, b = 0.021, a = 1},
@@ -15,18 +22,31 @@ local molten_copper = {
     type = "fluid",
     name = "molten-copper",
     icon = "__ParadoxTech__/graphics/icons/molten_copper.png",
-    icon_size = 32,
+    icons = {
+        {
+            icon = table.deepcopy(dropletIcon),
+            tint = {r = 1, g = 128/255, b = 0, a = 1},
+            icon_size = 32
+        }
+    },
     default_temperature = 1538,
     max_temperature = 2862,
     base_color = {r = 0.402, g = 0.068, b = 0.020, a = 1},
     flow_color = {r = 0.402, g = 0.068, b = 0.020, a = 1}
 }
 
+local airIcon = data.raw.fluid["steam"].icon
+
 local compressed_air = {
     type = "fluid",
     name = "compressed-air",
-    icon = "__ParadoxTech__/graphics/icons/molten_copper.png",
-    icon_size = 32,
+    icons = {
+        {
+            icon = table.deepcopy(airIcon),
+            tint = {r = 0.85, g = 0.85, b = 1, a = 0.5},
+            icon_size = 32
+        }
+    },
     default_temperature = 20,
     max_temperature = 1000,
     base_color = {r = 1, g = 1, b = 1, a = 0.3},
@@ -36,8 +56,13 @@ local compressed_air = {
 local hot_air = {
     type = "fluid",
     name = "hot-air",
-    icon = "__ParadoxTech__/graphics/icons/molten_copper.png",
-    icon_size = 32,
+    icons = {
+        {
+            icon = table.deepcopy(airIcon),
+            tint = {r = 1, g = 128/255, b = 0, a = 0.5},
+            icon_size = 32
+        }
+    },
     default_temperature = 1000,
     max_temperature = 5000,
     base_color = {r = 1, g = 151/255, b = 74/255, a = 0.5},
